a257feb22414daa2495dc817f887e102141c74f8
apierre42 <aurel.chenuil@gmail.com>
2015-02-27 11:24:17 +0100

    ft_putnbr.c

---
+++ sources/ft_stdio/ft_putnbr.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+void	ft_putnbr(int n)
+{
+	char c;
+
+	if (n == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		n = -n;
+		write(1, "-", 1);
+	}
+	if (n < 10)
+	{
+		c = n + '0';
+		write(1, &c, 1);
+	}
+	else
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+}
