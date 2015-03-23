e6a131d806586081c30ba1add4893f2e66d1827e
apierre42 <aurel.chenuil@gmail.com>
2015-03-23 14:07:57 +0100

    ft_stdio.h

---
+++ includes/ft_stdio.h
@@ -0,0 +1,13 @@
+#ifndef FT_STDIO_H
+# ifndef FT_STDIO_H
+
+void	ft_putchar(char c);
+void	ft_putchar_fd(char c);
+void	ft_putendl(char const *s);
+void	ft_putendl_fd(char const *s, int fd);
+void	ft_putnbr(int n);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr(char const *s);
+void	ft_putstr_fd(char const *s, int fd);
+
+#endif
