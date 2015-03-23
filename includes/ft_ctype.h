71dee5446b1ffc37a096617a75604babac952da8
apierre42 <aurel.chenuil@gmail.com>
2015-03-23 14:01:00 +0100

    ft_ctype.h

---
+++ includes/ft_ctype.h
@@ -0,0 +1,14 @@
+#ifndef FT_CTYPE_H
+# define FT_CTYPE_H
+
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isblank(int c);
+int	ft_isdigit(int c);
+int	ft_ispunct(int c);
+int	ft_isspace(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+
+#endif
